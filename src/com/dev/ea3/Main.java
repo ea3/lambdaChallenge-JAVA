64b2ff9d79c24e0a6621c878ffb2685ab65a67a7
Emilio Araos <ea3dev@gmail.com>
2019-11-26 22:43:05 -0700

    setting up repo

---
+++ src/com/dev/ea3/Main.java
@@ -0,0 +1,52 @@
+package com.dev.ea3;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String myString = "Let's split this into an array";
+                String[] parts = myString.split(" ");
+                for (String part : parts){
+                    System.out.println(part);
+                }
+            }
+        };
+
+	    Runnable runnable1 = () -> {
+            String myString = "Let's split this into an array";
+            String[] parts = myString.split(" ");
+            for (String part : parts){
+                System.out.println(part);
+            }
+        };
+
+        Function<String, String> lambdaFunction = s -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i<s.length(); i++){
+                if( i % 2 == 1){
+                    returnVal.append(s.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        System.out.println(lambdaFunction.apply("1234567890"));
+        String result = everySecondCharacter(lambdaFunction, "1234567890");
+        System.out.println(result);
+
+//        Supplier<String> iLoveJava = () -> "I Love Java";
+        Supplier<String> iLoveJava = () -> {return "I Love Java";};
+        String supplierResult = iLoveJava.get();
+        System.out.println(supplierResult);
+
+    }
+
+    public static String everySecondCharacter(Function<String, String> func, String source){
+        return func.apply(source);
+    }
+}
